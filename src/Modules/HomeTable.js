9d52bcaf4a6f88d33cd8c362b675ab1203ea2a9c
“João <joaovlsaro@gmail.com>
2019-07-18 16:24:41 +0100

    Chips with logic

--- src/Modules/HomeTable.js
+++ src/Modules/HomeTable.js
@@ -1,30 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Chip, H4, Icon, Paragraph, Table } from 'cobalt-react-components';
+import getMajorVersion from '../helpers/getMajorVersion'
 import '../styles.css';
+
+const COBALT_VERSION = '27.0.0'
 
 const onSortDirectionChange = () => {} // TO DO
 
-const RepoRow = ({title, description, link, repoVersion}) => (
-  <Table.Row>
-    <Table.Data truncated>
-      <Link to={link}>
-        <H4>
-          {title}
-          <Chip warning>{repoVersion}</Chip>
-        </H4>
-        <Paragraph truncated microcopy>{description}</Paragraph>
-      </Link>
-    </Table.Data>
-    <Table.ActionData>
-      <Chip warning>Lagging</Chip>
-      <Button small>
-        <Icon name={Icon.OPEN_IN_NEW} />
-      </Button>
-      <Link to={link} className='co-button co--primary co--small co--invert'>View details</Link>
-    </Table.ActionData>
-  </Table.Row>
-)
+const RepoRow = ({title, description, link, repoVersion}) => {
+  const versionLag = getMajorVersion(COBALT_VERSION) - getMajorVersion(repoVersion);
+
+  const statusProps = (versionLag) => (
+    {
+      success: versionLag === 0,
+      warning: versionLag > 0 && versionLag <= 2,
+      danger: versionLag > 2,
+    }
+  )
+
+  const stateText = versionLag === 0
+    ? 'Updated'
+    : versionLag <= 2
+      ? 'Lagging'
+      : 'Outdated'
+
+  return (
+    <Table.Row>
+      <Table.Data truncated>
+        <Link to={link}>
+          <H4>
+            {title}
+            <Chip {...statusProps(versionLag)}>{repoVersion}</Chip>
+          </H4>
+          <Paragraph truncated microcopy>{description}</Paragraph>
+        </Link>
+      </Table.Data>
+      <Table.ActionData>
+        <Chip {...statusProps(versionLag)}>{stateText}</Chip>
+        <Button small>
+          <Icon name={Icon.OPEN_IN_NEW} />
+        </Button>
+        <Link to={link} className='co-button co--primary co--small co--invert'>View details</Link>
+      </Table.ActionData>
+    </Table.Row>
+  )
+}
 
 const HomeTable = () => {
   return (
@@ -45,31 +66,31 @@ const HomeTable = () => {
         <RepoRow
           title='Repo 1'
           description='Description 1'
-          link='/project-components/'
+          link='#'
           repoVersion='27.0.0'
         />
         <RepoRow
           title='Repo 2'
           description='Description 2'
-          link='/project-components/'
+          link='#'
           repoVersion='26.0.0'
         />
         <RepoRow
           title='Repo 3'
           description='Description 3'
-          link='/project-components/'
+          link='#'
           repoVersion='25.0.0'
         />
         <RepoRow
           title='Repo 4'
           description='Description 4'
-          link='/project-components/'
+          link='#'
           repoVersion='24.0.0'
         />
         <RepoRow
           title='Repo 5'
           description='Description 5'
-          link='/project-components/'
+          link='#'
           repoVersion='23.0.0'
         />
       </Table.Body>
