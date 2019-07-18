bb7768d98ed45207a8046ed976deedfd8877dfd6
“João <joaovlsaro@gmail.com>
2019-07-18 12:39:04 +0100

    Add CobaltRoot

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import CobaltRoot, { Button } from 'cobalt-react-components';
 import Home from './Views/Home';
 import Repo from './Views/Repo';
+import './index.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/repo">Repo</Link>
-        </li>
-      </ul>
-    </nav>
-    <div>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/repo/" component={Repo}/>
-    </div>
-  </BrowserRouter>,
+  <CobaltRoot>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/repo">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/repo/" component={Repo}/>
+        <Button primary onClick={() => {}}>Yeye</Button>
+      </div>
+    </BrowserRouter>
+  </CobaltRoot>,
   document.getElementById('root')
 );
 
