58a2348b3927530a23c5f79d417bfcc8c726520b
mmelo <miguelbritomelo@gmail.com>
2019-07-18 17:52:44 +0100

    landing page api integration

--- src/api/projects.js
+++ src/api/projects.js
@@ -5,31 +5,41 @@ const PROJECTS = {
         "projectName": "App Connect - Marketplace-UI (Launcher and Wizard)",
         "cobaltReactVersion": "22.0.0",
         "cobaltDesignVersion": "21.0.0",
-        "packageLink": "https://github.com/Talkdesk/marketplace-ui/blob/master/package.json#L140"
+        "packageLink": "https://github.com/Talkdesk/marketplace-ui/blob/master/package.json#L140",
+        "repoLink": "https://github.com/Talkdesk/marketplace-ui/",
+        "repoPartial": "marketplace-ui/"
       },
       {
         "projectName": "Studio (CXM)",
         "cobaltReactVersion": "27.0.0",
         "cobaltDesignVersion": "24.0.0",
-        "packageLink": "https://github.com/Talkdesk/cxm-ui/blob/master/package.json#L12"
+        "packageLink": "https://github.com/Talkdesk/cxm-ui/blob/master/package.json#L12",
+        "repoLink": "https://github.com/Talkdesk/cxm-ui/",
+        "repoPartial": "cmx-ui/"
       },
       {
         "projectName": "Studio Backoffice",
         "cobaltReactVersion": "21.1.1",
         "cobaltDesignVersion": "19.0.3",
-        "packageLink": "https://github.com/Talkdesk/studio-backoffice-ui/blob/master/package.json#L8"
+        "packageLink": "https://github.com/Talkdesk/studio-backoffice-ui/blob/master/package.json#L8",
+        "repoLink": "https://github.com/Talkdesk/studio-backoffice-ui/",
+        "repoPartial": "studio-backoffice-ui/"
       },
       {
         "projectName": "Reporting Explore UI",
         "cobaltReactVersion": "24.0.1",
         "cobaltDesignVersion": "21.7.0",
-        "packageLink": "https://github.com/Talkdesk/reporting-explore-ui/blob/master/package.json#L32"
+        "packageLink": "https://github.com/Talkdesk/reporting-explore-ui/blob/master/package.json#L32",
+        "repoLink": "https://github.com/Talkdesk/reporting-explore-ui/",
+        "repoPartial": "reporting-explore-ui/"
       },
       {
         "projectName": "Reporting Live Dashboards UI",
         "cobaltReactVersion": "26.0.0",
         "cobaltDesignVersion": "23.0.0",
-        "packageLink": "https://github.com/Talkdesk/reporting-live-dashboards-ui/blob/master/package.json#L28"
+        "packageLink": "https://github.com/Talkdesk/reporting-live-dashboards-ui/blob/master/package.json#L28",
+        "repoLink": "https://github.com/Talkdesk/reporting-live-dashboards-ui/",
+        "repoPartial": "reporting-live-dashboard-ui/"
       }
     ]
   }
