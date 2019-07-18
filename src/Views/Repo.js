128c782e0e56265e06153244ef617b4b585fbfd4
“João <joaovlsaro@gmail.com>
2019-07-18 12:36:06 +0100

    config react-router

---
+++ src/Views/Repo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../styles.css';
+
+function Home() {
+  return (
+    <div>
+     repo
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Views/Home';
+import Repo from './Views/Repo';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/repo">Repo</Link>
+        </li>
+      </ul>
+    </nav>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/repo/" component={Repo}/>
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
