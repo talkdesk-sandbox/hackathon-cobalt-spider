dfb86b9518c926328a8194092d44af31ce5737da
Pedro Nogueira <pedro.nogueira@talkdesk.com>
2019-07-18 23:10:25 +0100

    Adds Projects Component List

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -7,13 +7,14 @@ import '../styles.css';
 import { projectAPI, repoAPI } from '../api/index';
 
 function getVersion(data) {
-  const { devDependencies } = JSON.parse(window.atob(data.content));
+  return '27.0.0'
+  // const { devDependencies } = JSON.parse(window.atob(data.content));
 
-  if (!devDependencies) return null;
+  // if (!devDependencies) return null;
 
-  return devDependencies.hasOwnProperty('cobalt-react-components')
-    ? devDependencies['cobalt-react-components']
-    : null;
+  // return devDependencies.hasOwnProperty('cobalt-react-components')
+  //   ? devDependencies['cobalt-react-components']
+  //   : null;
 }
 
 class Home extends React.Component {
--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import GitHub from 'github-api';
 
 // const CO_REACT_VERSION_REPO = 'cobalt-react-components';
 // const GITHUB_OAUTH_TOKEN = '80bb6ce8fd5768401637ad491fae34cfee65c2bd';
-const GITHUB_OAUTH_TOKEN = '';
+const GITHUB_OAUTH_TOKEN = '9d71b5470f8919085747060d739cb5f95db49bb2';
 // const defaultPlaceholderVersions = {
 //   cobaltReactLatestVersion: '27.0.0'
 // };
