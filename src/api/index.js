4501a7bc8c43a3886419b5029e7094e304317626
mmelo <miguelbritomelo@gmail.com>
2019-07-18 21:31:46 +0100

    reduce number of github api requests

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import projects from './projects';
 
 const API_URL = 'https://api.github.com/';
 const CO_REACT_VERSION_REPO = 'cobalt-react-components';
-const GITHUB_OAUTH_TOKEN = 'e68416cb10d3e6b847d331940f6636ae2a0abb43';
+const GITHUB_OAUTH_TOKEN = '80bb6ce8fd5768401637ad491fae34cfee65c2bd';
 const defaultPlaceholderVersions = {
   cobaltReactLatestVersion: '27.0.0'
 };
@@ -13,13 +13,18 @@ const repoURLGenerator = (repo) => {
 };
 
 const fetchVersion = async () => {
+  const currentVersion = window.localStorage.getItem('cobalt_react_components_current_version');
+
+  if(currentVersion) return { cobaltReactLatestVersion: currentVersion };
+
   try {
     const co_react_api_call = await fetch(repoURLGenerator(CO_REACT_VERSION_REPO));
     const co_react_data = await co_react_api_call.json();
+    const cobaltReactLatestVersion = co_react_data.tag_name.replace('v', '');
 
-    return {
-      cobaltReactLatestVersion: co_react_data.tag_name.replace('v', '')
-    };
+    window.localStorage.setItem('cobalt_react_components_current_version', cobaltReactLatestVersion);
+
+    return { cobaltReactLatestVersion };
   } catch (error) {
     console.log(error);
 
