8b2292ad26960b4323823ede4711e7b608995232
Carlos Silva <macasilva1@gmail.com>
2019-07-19 12:27:10 +0100

    fix scrool changes

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import GitHub from 'github-api';
 
 // const CO_REACT_VERSION_REPO = 'cobalt-react-components';
 // const GITHUB_OAUTH_TOKEN = '80bb6ce8fd5768401637ad491fae34cfee65c2bd';
-const GITHUB_OAUTH_TOKEN = '';
+const GITHUB_OAUTH_TOKEN = '05ffe0735201480f20dc973b74e968f8dbc2043d';
 // const defaultPlaceholderVersions = {
 //   cobaltReactLatestVersion: '27.0.0'
 // };
